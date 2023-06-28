e6a97b20137b8758149dc7b5ab1de3bc23d16bdf
ioangheraszim <gijohny17@gmail.com>
2023-06-28 13:22:19 +0100

    added a component some styles and an image to the project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <nav className="nav-bar">
+            <div className="left-nav">
+                <img src="./src/assets/troll-face.png" alt="navbar-icon" />
+                <h1>Meme Generator</h1>
+            </div>
+            <p>React Course - Project 6</p>
+        </nav>
+    )
+   
+}
